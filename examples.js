5530f954052472342162a1832d7f7ce705e2feb3
edsilv <e.silverton@gmail.com>
2014-05-19 14:35:11 +0100

    each

--- examples.js
+++ examples.js
@@ -4,6 +4,10 @@ if (document.location.href.indexOf('localhost') != -1){
     document.write('<script type="text/javascript" id="embedWellcomePlayer" src="/src/js/embed.js"><\/script>');
 } else {
     // built version
-    $('.wellcomePlayer').data('config', '/examples.config');
+
+    $.each($('.wellcomePlayer'), function(index, value) {
+      $(value).data('config', '/examples.config');
+    });
+
     document.write('<script type="text/javascript" id="embedWellcomePlayer" src="/build/wellcomeplayer/js/embed.js"><\/script>');
 }
