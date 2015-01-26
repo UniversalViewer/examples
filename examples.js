1afb50226736434f7fb45f5b354c9fcd11d431ac
Ken Tsang <ken.tsang@bl.uk>
2015-01-26 11:51:37 +0000

    build 0.1.31

--- examples.js
+++ examples.js
@@ -484,7 +484,7 @@ $(function(){
     var editor;
 
     if (testBuild){
-        $("body").append('<script type="text/javascript" id="embedUV" src="/build/uv-0.1.30/js/embed.js"><\/script>');
+        $("body").append('<script type="text/javascript" id="embedUV" src="/build/uv-0.1.31/js/embed.js"><\/script>');
     } else {
         if (isLocalhost){
             $("body").append('<script type="text/javascript" id="embedUV" src="/src/js/embed.js"><\/script>');
@@ -506,7 +506,7 @@ $(function(){
                 $(this).updateAttr('value', '/examples/', '/');
             });
 
-            $("body").append('<script type="text/javascript" id="embedUV" src="/build/uv-0.1.30/js/embed.js"><\/script>');
+            $("body").append('<script type="text/javascript" id="embedUV" src="/build/uv-0.1.31/js/embed.js"><\/script>');
         }
     }
 
@@ -611,7 +611,7 @@ $(function(){
 
             // first get the default extension config
             // todo: figure out how to make this work for more than just seadragon extension
-            $.getJSON('/build/uv-0.1.30/js/coreplayer-seadragon-extension-config.js', function(baseConfig){
+            $.getJSON('/build/uv-0.1.31/js/coreplayer-seadragon-extension-config.js', function(baseConfig){
                 var configUrl = $('#config option:selected').val();
 
                 $.getJSON(configUrl, function(config){
