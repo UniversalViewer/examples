d1b38cbdec231cf37c89815e6e30cb7e38b05b15
edsilv <e.silverton@gmail.com>
2018-07-12 21:07:35 +0100

    build

--- uv/lib/iiif-av-component.js
+++ uv/lib/iiif-av-component.js
@@ -1,4 +1,4 @@
-// iiif-av-component v0.0.58 https://github.com/iiif-commons/iiif-av-component#readme
+// iiif-av-component v0.0.59 https://github.com/iiif-commons/iiif-av-component#readme
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.iiifAvComponent = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){
 
@@ -432,6 +432,7 @@ var IIIFComponents;
                 value: that._data.volume,
                 step: 0.1,
                 orientation: "horizontal",
+                range: "min",
                 min: 0,
                 max: 1,
                 animate: false,
@@ -767,6 +768,11 @@ var IIIFComponents;
                 };
                 this._renderMediaElement(itemData);
             }
+        };
+        CanvasInstance.prototype.data = function () {
+            return {
+                volume: 1
+            };
         };
         CanvasInstance.prototype.isVirtual = function () {
             return this._data.canvas instanceof IIIFComponents.AVComponentObjects.VirtualCanvas;
