9fcb0c0d037069fecf99ecf1b3490bc6a5d74d90
Edward Silverton <e.silverton@gmail.com>
2021-06-08 16:43:21 +0100

    build

--- uv/lib/uv-seadragon-extension-dependencies.js
+++ uv/lib/uv-seadragon-extension-dependencies.js
@@ -1,7 +1,8 @@
 define(function () {
     return function (formats) {
         return {
-            async: ['TreeComponent', 'GalleryComponent', 'MetadataComponent', 'openseadragon.min']
+            async: ['TreeComponent', 'GalleryComponent', 'IIIFMetadataComponent', 'openseadragon.min']
         };
     };
 });
+//# sourceMappingURL=dependencies.js.map
